5da8f4683c7007f769d190fff53445641e554e9c
unknown <valeria.bermudez-a@mail.escuelaing.edu.co>
2025-09-29 14:34:19 -0500

    reacion Proyecto Spring

---
+++ furnistore/src/main/java/com/furnistore/furnistore/FurnistoreApplication.java
@@ -0,0 +1,13 @@
+package com.furnistore.furnistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnistoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnistoreApplication.class, args);
+	}
+
+}
---
+++ furnistore/src/test/java/com/furnistore/furnistore/FurnistoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furnistore.furnistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnistoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
